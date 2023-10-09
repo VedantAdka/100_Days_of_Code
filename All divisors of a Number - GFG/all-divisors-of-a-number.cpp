8a3c7ccae3684bbc90dbb234a8f1ad004804411f
VedantAdka <117341817+VedantAdka@users.noreply.github.com>
2023-10-09 14:52:22 +0530

    Added solution - LeetHub

---
+++ All divisors of a Number - GFG/all-divisors-of-a-number.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    void print_divisors(int n) {
+        vector<int>ls;
+        for(int i=1;i<=sqrt(n);i++){
+            if(n%i==0){
+                ls.push_back(i);
+                if(n/i!=i){
+                    ls.push_back(n/i);
+                }
+            }
+        }
+        sort(ls.begin(),ls.end());
+        for(auto it:ls){
+            cout<<it<<" ";
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        ob.print_divisors(n);
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends

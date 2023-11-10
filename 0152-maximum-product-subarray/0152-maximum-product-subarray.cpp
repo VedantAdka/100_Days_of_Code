0599336767ed25b580382400bfb27cf71fcb90ca
VedantAdka <117341817+VedantAdka@users.noreply.github.com>
2023-11-10 10:56:09 +0530

    Time: 10 ms (16.86%), Space: 14.4 MB (9.16%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -6,12 +6,8 @@ public:
         int pre=1;
         int suff=1;
         for(int i=0;i<n;i++){
-            if(pre==0){
-                pre=1;
-            }
-            if(suff==0){
-                suff=1;
-            }
+            if(pre==0){pre=1;}
+            if(suff==0){suff=1;}
             pre=pre*nums[i];
             suff=suff*nums[n-i-1];
             maxi=max(maxi,max(pre,suff));

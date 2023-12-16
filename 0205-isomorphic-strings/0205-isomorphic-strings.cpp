b9a9a5930eac81cedf27657a44a342f42102e890
VedantAdka <117341817+VedantAdka@users.noreply.github.com>
2023-12-16 15:14:25 +0530

    Time: 6 ms (59.85%), Space: 7.4 MB (53.33%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -5,22 +5,17 @@ public:
         if (str1.length() != str2.length()) {
         return false;
     }
-
     std::unordered_map<char, char> mapping;
     std::unordered_map<char, bool> usedCharacters;
 
     for (int i = 0; i < str1.length(); ++i) {
         char char1 = str1[i];
         char char2 = str2[i];
-
-        // If char1 is mapped to a different character in str2, or char2 is already mapped, return false
         if (mapping.find(char1) != mapping.end() && mapping[char1] != char2) {
             return false;
-        } else if (mapping.find(char1) == mapping.end() && usedCharacters[char2]) {
+        } else if (mapping.find(char1) == mapping.end() && usedCharacters[char2])         {
             return false;
         }
-
-        // Map char1 to char2
         mapping[char1] = char2;
         usedCharacters[char2] = true;
     }

bd161d92810b8c0fff0ccd494e87c31167940270
VedantAdka <117341817+VedantAdka@users.noreply.github.com>
2024-07-21 11:35:05 +0530

    Time: 24 ms (38.16%), Space: 45 MB (40.69%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -31,5 +31,34 @@ class Solution {
             }
         }
         return dp[n][m];
+        // boolean[] prev=new boolean[m+1];
+        // boolean[] curr=new boolean[m+1];
+        // prev[0]=true;
+        // for(int j=1;j<=m;j++){
+        //     prev[j]=false;
+        // }
+        // for(int i=1;i<=n;i++){
+        //     boolean flag=true;
+        //     for(int ii=1;ii<=i;ii++){
+        //         if(s.charAt(ii-1)!='*'){
+        //             flag=false;
+        //             break;
+        //         }
+        //     }
+        //     curr[0]=flag;
+        //     for(int j=1;j<=m;j++){
+        //         if(s.charAt(i-1)==p.charAt(j-1) || s.charAt(i-1)=='?'){
+        //             curr[j]=prev[j-1];
+        //         }
+        //         else if(s.charAt(i-1)=='*'){
+        //             curr[j]=prev[j] || curr[j-1];
+        //         }
+        //         else{
+        //             curr[j]=false;
+        //         }
+        //     }
+        //     prev=curr;
+        // }
+        // return prev[m];
     }
 }

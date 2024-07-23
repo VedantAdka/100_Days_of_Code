c0358df8bc5d9af4f3dca323058ff42df808c29c
VedantAdka <117341817+VedantAdka@users.noreply.github.com>
2024-07-23 11:59:03 +0530

    Time: 21 ms (41.00%), Space: 54.8 MB (75.38%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -3,14 +3,16 @@ class Solution {
         int n=a.length;
         int[][] dp=new int[n+1][2];
         for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                if(buy==1){
-                    dp[i][buy]=Math.max(-a[i]+dp[i+1][0],0+dp[i+1][1]);
-                }
-                else{
-                    dp[i][buy]=Math.max(a[i]+dp[i+1][1]-fee,0+dp[i+1][0]);
-                }
-            }
+            dp[i][1]=Math.max(-a[i]+dp[i+1][0],0+dp[i+1][1]);
+            dp[i][0]=Math.max(a[i]+dp[i+1][1]-fee,0+dp[i+1][0]);
+            // for(int buy=0;buy<=1;buy++){
+            //     if(buy==1){
+            //         dp[i][buy]=Math.max(-a[i]+dp[i+1][0],0+dp[i+1][1]);
+            //     }
+            //     else{
+            //         dp[i][buy]=Math.max(a[i]+dp[i+1][1]-fee,0+dp[i+1][0]);
+            //     }
+            // }
         }
         return dp[0][1];
     }

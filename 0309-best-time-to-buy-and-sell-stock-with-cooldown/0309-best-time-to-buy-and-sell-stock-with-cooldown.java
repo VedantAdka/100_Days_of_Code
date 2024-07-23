a97e9d1f59598e335ab2971ed93b6486f72f4476
VedantAdka <117341817+VedantAdka@users.noreply.github.com>
2024-07-23 12:00:14 +0530

    Time: 1 ms (82.84%), Space: 41.4 MB (85.22%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -14,10 +14,16 @@ class Solution {
         int n=a.length;
         int[][] dp=new int[n+2][2];
         for(int i=n-1;i>=0;i--){
-            for(int buy=0;buy<=1;buy++){
-                dp[i][1]=Math.max(-a[i]+dp[i+1][0],0+dp[i+1][1]);
-                dp[i][0]=Math.max(a[i]+dp[i+2][1],0+dp[i+1][0]);
-            }
+            dp[i][1]=Math.max(-a[i]+dp[i+1][0],0+dp[i+1][1]);
+            dp[i][0]=Math.max(a[i]+dp[i+2][1],0+dp[i+1][0]);
+            // for(int buy=0;buy<=1;buy++){
+            //     if(buy==1){
+            //         dp[i][buy]=Math.max(-a[i]+dp[i+1][0],0+dp[i+1][1]);
+            //     }
+            //     else{
+            //         dp[i][buy]=Math.max(a[i]+dp[i+2][1],0+dp[i+1][0]);
+            //     }
+            // }
         }
         return dp[0][1];
         //return f(0,1,a);

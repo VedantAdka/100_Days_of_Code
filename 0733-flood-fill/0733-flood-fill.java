ef470bf30a364d7c167aacc0719757800e805266
VedantAdka <117341817+VedantAdka@users.noreply.github.com>
2024-08-10 18:37:06 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (46.36%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,26 +1,32 @@
 class Solution {
-    public boolean isValid(int[][] image, int sr, int sc, int org){
-        return(sr>=0 && sr<image.length && sc>=0 && sc<image[0].length && image[sr][sc]==org) ;       
+    public boolean isValid(int[][] image, int sr, int sc, int org) {
+        // Ensure within bounds and the pixel matches the original color
+        return sr >= 0 && sr < image.length && sc >= 0 && sc < image[0].length && image[sr][sc] == org;
     }
-    public void f(int[][] image, int i, int j, int color,int org){
-        image[i][j]=color;
-        if(isValid(image,i+1,j,org)){
-            f(image,i+1,j,color,org);
+
+    public void f(int[][] image, int sr, int sc, int color, int org) {
+        // Color the current pixel
+        image[sr][sc] = color;
+        
+        // Recursively color neighboring pixels if they are valid
+        if (isValid(image, sr + 1, sc, org)) {
+            f(image, sr + 1, sc, color, org);
         }
-        if(isValid(image,i-1,j,org)){
-            f(image,i-1,j,color,org);
+        if (isValid(image, sr - 1, sc, org)) {
+            f(image, sr - 1, sc, color, org);
         }
-        if(isValid(image,i,j+1,org)){
-            f(image,i,j+1,color,org);
+        if (isValid(image, sr, sc + 1, org)) {
+            f(image, sr, sc + 1, color, org);
         }
-        if(isValid(image,i,j-1,org)){
-            f(image,i,j-1,color,org);
+        if (isValid(image, sr, sc - 1, org)) {
+            f(image, sr, sc - 1, color, org);
         }
     }
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int org=image[sr][sc];
-        if(org!=color){
-            f(image,sr,sc,color,org);
+        int org = image[sr][sc];
+        if (org != color) {
+            f(image, sr, sc, color, org);
         }
         return image;
     }

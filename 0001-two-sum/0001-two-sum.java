2f299f09a5bf30b578e95c93336a1ef2c8d8bd07
VedantAdka <117341817+VedantAdka@users.noreply.github.com>
2024-09-05 11:22:22 +0530

    Time: 6 ms (55.09%), Space: 44.6 MB (80.00%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,39 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> m=new HashMap<>();
-        int[] ans={-1,-1};
-        for(int i=0;i<nums.length;i++){
-            if(m.containsKey(target-nums[i])){
-                ans[0]=i;
-                ans[1]=m.get(target-nums[i]);
-                return ans;
+        // Map<Integer,Integer> m=new HashMap<>();
+        // int[] ans={-1,-1};
+        // for(int i=0;i<nums.length;i++){
+        //     if(m.containsKey(target-nums[i])){
+        //         ans[0]=i;
+        //         ans[1]=m.get(target-nums[i]);
+        //         return ans;
+        //     }
+        //     else{
+        //         m.put(nums[i],i);
+        //     }
+        // }
+        // return ans;
+        int[] ans=new int[2];
+        ans[0]=-1;
+        ans[1]=-1;
+        ArrayList<Integer> a=new ArrayList<>();
+        for(int x: nums){
+            a.add(x);
+        }
+        Arrays.sort(nums);
+        int l=0;
+        int r=nums.length-1;
+        while(l<r){
+            if(nums[l]+nums[r]>target){
+                r--;
+            }
+            else if(nums[l]+nums[r]<target){
+                l++;
             }
             else{
-                m.put(nums[i],i);
+                ans[0]=a.indexOf(nums[l]);
+                ans[1]=a.lastIndexOf(nums[r]);
+                break;
             }
         }
         return ans;
